404f7d2eb93343fcd885c6b97af121de536c42aa
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2021-11-24 17:04:30 +0100

    linting and refactoring

--- fmri/acquisitions/non_cartesian.py
+++ fmri/acquisitions/non_cartesian.py
@@ -26,119 +26,158 @@ class SparklingAcquisition(BaseFMRIAcquisition):
     -----
         The sampling pattern is assumed to be constant in time, and store in a Sparkling compliant format.
 
-    Parameters
+    Attributes
     ----------
-    data_file: str
-        `.dat` file in twix format , containing the experimental data.
-    trajectory_file: str
-        `.bin` file containing kspace_locations of Sparkling acquisition
-    smaps_file: str (optional, default None)
-        sensitivity profiles of coils. If empty and calibration is not False, they will be computed.
-    frame_slicer: slice (optional, default None)
-        Time frame selector, use it to reduce the memory footprint by only importing a subset of slices.
-    load_data: bool (optional, default True)
-        Flag to load data.
-    normalize: bool (optional, default True)
-        Flag to normalize the kspace frequencies between -0.5 and 0.5
-    bin_load_kwargs: dict (optional, default None)
-        Extra argument to load trajectory files.
-        If not provided, the trajectory file will be imported once to extract correct parameters.
-    calibration_kwargs: dict (optional, default None)
-        Calibration parameters use to estimate the smaps if not provided.
+    kspace_data: ndarray
+        data acquired, shaped: (n_frames, n_coils, n_shots*n_samples)
+    kspace_loc: ndarray
+        samples points for the acquired data, shape (n_frame, n_shots*n_samples, DIM)
+    n_frames: int
+        Number of temporal frames of data acquisition
+    n_coils: int
+        Number of coils
+    n_shots: int
+        Number of shots
+    n_samples: int
+        Number of samples per shots
+    FOV: ndarray
+        Field of view in meters, for each dimension of the image
+    img_shape: ndarray
+        Size in pixel/voxel of the image
+    OSF: int
+        Oversampling factor
+    density_comp: ndarray
+        Density compensation estimation, computed and cached on the fly.
     """
+
     def __init__(self, data_file, trajectory_file, smaps_file=None, shifts=None,
-                 frame_slicer=None, normalize=True, bin_load_kwargs:dict=None) -> None:
+                 frame_slicer=None, normalize=True, bin_load_kwargs: dict=None) -> None:
+        """
+        Init SparklingAcquisition.
+
+        Parameters
+        ----------
+        data_file: str
+            `.dat` file in twix format , containing the experimental data.
+        trajectory_file: str
+            `.bin` file containing kspace_locations of Sparkling acquisition
+        smaps_file: str (optional, default None)
+            sensitivity profiles of coils. If empty and calibration is not False, they will be computed.
+        frame_slicer: slice (optional, default None)
+            Time frame selector, use it to reduce the memory footprint by only importing a subset of slices.
+        load_data: bool (optional, default True)
+            Flag to load data.
+        normalize: bool (optional, default True)
+            Flag to normalize the kspace frequencies between -0.5 and 0.5
+        bin_load_kwargs: dict (optional, default None)
+            Extra argument to load trajectory files.
+            If not provided, the trajectory file will be imported once to extract correct parameters.
+        calibration_kwargs: dict (optional, default None)
+            Calibration parameters use to estimate the smaps if not provided.
+
+        """
         self._data_file = data_file
         self._traj_file = trajectory_file
         self._smaps_file = smaps_file
         self.shifts = shifts
-    
+
         # load .dat infos
         _twix_obj = mapVBVD(self._data_file)
         traj_name = _twix_obj.hdr['Meas']['tFree']
         if self._traj_file is None:
-            self._traj_file = os.path.join(os.path.dirname(self._data_file),traj_name)
-            
-        
+            self._traj_file = os.path.join(
+                os.path.dirname(self._data_file), traj_name)
+
         if traj_name not in self._traj_file:
-            warnings.warn("The trajectory file specified in data_file is probably not the same as the one provided, using the latter.")
+            warnings.warn(
+                "The trajectory file specified in data_file is probably not the same as the one provided, using the latter.")
         self._twix_obj = _twix_obj
-        self._twix_obj.image.flagRemoveOS= False
+        self._twix_obj.image.flagRemoveOS = False
         self._twix_obj.image.squeeze = True
-        
+
         if len(self._twix_obj.image.sqzSize) > 4:
             raise ValueError("Data import with averaging is not available.")
         if frame_slicer is not None:
-            self.kspace_data = self._twix_obj.image[:,:,:,frame_slicer]
+            self.kspace_data = self._twix_obj.image[:, :, :, frame_slicer]
         else:
             self.kspace_data = self._twix_obj.image[""]
 
-        self.kspace_data = self.kspace_data.swapaxes(1,2)
-        self.kspace_data = self.kspace_data.swapaxes(0,1)
-        
-        self.kspace_data = np.reshape(self.kspace_data, (self.kspace_data.shape[0]*self.kspace_data.shape[1], 
-                                               self.kspace_data.shape[2], self.kspace_data.shape[3]))
+        self.kspace_data = self.kspace_data.swapaxes(1, 2)
+        self.kspace_data = self.kspace_data.swapaxes(0, 1)
+
+        self.kspace_data = np.reshape(self.kspace_data,
+                                      (self.kspace_data.shape[0]*self.kspace_data.shape[1],
+                                       self.kspace_data.shape[2], self.kspace_data.shape[3]))
         self.kspace_data = self.kspace_data.T
-        
-        
-        
+
         # load .bin data and infos.
         if bin_load_kwargs is None:
             # HACK: the infos and data are retrieve twice to ensure the correct sampling rate.
-            self.kspace_loc, infos = get_kspace_loc_from_gradfile(self._traj_file)
-            self.kspace_loc, _     = get_kspace_loc_from_gradfile(self._traj_file,
-                                                                dwell_time=0.01/float(infos['min_osf']), num_adc_samples=infos['num_samples_per_shot']*infos['min_osf'])
+            self.kspace_loc, infos = get_kspace_loc_from_gradfile(
+                self._traj_file)
+            self.kspace_loc, _ = get_kspace_loc_from_gradfile(self._traj_file,
+                                                              dwell_time=0.01/float(infos['min_osf']),
+                                                              num_adc_samples=infos['num_samples_per_shot']*infos['min_osf'])
         else:
-            self.kspace_loc, infos = get_kspace_loc_from_gradfile(self._traj_file, **bin_load_kwargs)
-
+            self.kspace_loc, infos = get_kspace_loc_from_gradfile(
+                self._traj_file, **bin_load_kwargs)
 
         self.kspace_loc = np.reshape(self.kspace_loc,
                                      (self.kspace_loc.shape[0]*self.kspace_loc.shape[1],
                                       self.kspace_loc.shape[2]))
 
-        self.n_shots   = infos['num_shots']
+        self.n_shots = infos['num_shots']
         self.n_samples = infos['num_samples_per_shot']
-        self.FOV       = np.asarray(infos['FOV'])
-        self.DIM       = infos['dimension']
+        self.FOV = np.asarray(infos['FOV'])
+        self.DIM = infos['dimension']
         self.img_shape = np.asarray(infos['img_size'])
-        self.OSF       = infos['min_osf']
-        self.n_coils   = int(self._twix_obj.hdr['Meas']['NChaMeas'])
-        self.n_frames  = self.kspace_data.shape[0]
+        self.OSF = infos['min_osf']
+        self.n_coils = int(self._twix_obj.hdr['Meas']['NChaMeas'])
+        self.n_frames = self.kspace_data.shape[0]
 
         if normalize:
-            self.kspace_loc = normalize_frequency_locations(self.kspace_loc, Kmax=self.img_shape/(2*self.FOV))
+            self.kspace_loc = normalize_frequency_locations(
+                self.kspace_loc, Kmax=self.img_shape/(2*self.FOV))
 
-        self.kspace_data = add_phase_kspace(self.kspace_data,self.kspace_loc,shifts=shifts)
+        self.kspace_data = add_phase_kspace(
+            self.kspace_data, self.kspace_loc, shifts=shifts)
 
-    def get_smaps(self, use_rep=0, thresh=0.1, window=None, mode='gridding', method='linear', density_comp=None, n_cpu=MAX_CPU_CORE,ssos=False, **kwargs):
+    def get_smaps(self, use_rep=0, thresh=0.1, window_fun=None, mode='gridding',
+                method='linear', density_comp=None, n_cpu=MAX_CPU_CORE, ssos=False, **kwargs):
+        """Get Smaps from the center of kspace."""
         if type(thresh) is float:
             thresh = (thresh,)*self.DIM
-        data = self.kspace_data[use_rep,...]
+        data = self.kspace_data[use_rep, ...]
 
         Smaps, Ssos = get_Smaps(data,
-                             img_shape=self.img_shape,
-                             samples=self.kspace_loc,
-                             thresh=thresh,
-                             min_samples=self.kspace_loc.min(axis=0),
-                             max_samples=self.kspace_loc.max(axis=0),
-                             mode=mode,
-                             method=method,
-                             density_comp=density_comp,
-                             n_cpu=n_cpu,
-                             fourier_op_kwargs=kwargs)
+                                img_shape=self.img_shape,
+                                samples=self.kspace_loc,
+                                thresh=thresh,
+                                min_samples=self.kspace_loc.min(axis=0),
+                                max_samples=self.kspace_loc.max(axis=0),
+                                mode=mode,
+                                window_fun=window_fun,
+                                method=method,
+                                density_comp=density_comp,
+                                n_cpu=n_cpu,
+                                fourier_op_kwargs=kwargs)
         if ssos:
             return Smaps, Ssos
         return Smaps
 
-
     @property
     @functools.lru_cache(maxsize=None)
     def density_comp(self):
+        """
+        Estimate the density compensation.
+
+        The results is cached for performance.
+        """
         return estimate_density_compensation(self.kspace_loc, self.img_shape)
 
-
-    def get_fourier_operator(self, implementation='gpuNUFFT',**kwargs):
-            return NonCartesianFFT(samples=self.kspace_loc,
+    def get_fourier_operator(self, implementation='gpuNUFFT', **kwargs):
+        """Get the fourier operator associated to sampling pattern."""
+        return NonCartesianFFT(samples=self.kspace_loc,
                                shape=self.img_shape,
                                n_coils=self.n_coils,
                                implementation=implementation,
@@ -147,9 +186,11 @@ class SparklingAcquisition(BaseFMRIAcquisition):
                                )
 
     def save(self, filename):
-        np.savez(filename, kspace_data=self.kspace_data, kspace_loc=self.kspace_loc)
+        """Save the kspace_data and sampling pattern."""
+        np.savez(filename, kspace_data=self.kspace_data,
+                 kspace_loc=self.kspace_loc)
 
-    def __repr__(self) -> str :
+    def __repr__(self):
         return "SparklingAcquisition(\n"\
                f"shots={self.n_shots}\n"\
                f"samples/shots={self.n_samples}\n"\
@@ -160,6 +201,7 @@ class SparklingAcquisition(BaseFMRIAcquisition):
                f"FOV={self.FOV}\n"\
                f"Oversampling={self.OSF}\n"\
                f")"
+
 
 class CompressAcquisition(BaseFMRIAcquisition):
     """
--- fmri/reconstructors/frame_based.py
+++ fmri/reconstructors/frame_based.py
@@ -6,9 +6,9 @@ from joblib import Parallel, delayed
 import progressbar
 import numpy as np
 
-from mri.operators.gradient.gradient import GradAnalysis, GradSynthesis, GradSelfCalibrationAnalysis, GradSelfCalibrationSynthesis
+from mri.operators.gradient.gradient import GradAnalysis, GradSynthesis,\
+    GradSelfCalibrationAnalysis, GradSelfCalibrationSynthesis
 from mri.operators.fourier.non_cartesian import NonCartesianFFT
-from mri.operators.fourier.utils import estimate_density_compensation
 
 from mri.reconstructors.utils.extract_sensitivity_maps import get_Smaps
 from .base import BaseFMRIReconstructor
@@ -19,81 +19,102 @@ class SequentialFMRIReconstructor(BaseFMRIReconstructor):
     """ Sequential Reconstruction of fMRI data.
     Time frame are reconstructed in a row, the previous frame estimation is used as initialization for the next one."""
 
-    def __init__(self, fourier_op, space_linear_op, space_regularisation, optimizer="pogm",Smaps=None,verbose=0):
-        super().__init__(fourier_op, space_linear_op, space_regularisation, optimizer=optimizer,Smaps=Smaps,verbose=verbose)
+    def __init__(self, fourier_op, space_linear_op, space_regularisation, optimizer="pogm", Smaps=None, verbose=0):
+        super().__init__(fourier_op, space_linear_op, space_regularisation,
+                         optimizer=optimizer, Smaps=Smaps, verbose=verbose)
 
-    def get_grad_op(self, fourier_op=None):
-
-        fourier_op = self.fourier_op if fourier_op is None else fourier_op
-
+    def get_grad_op(self, **kwargs):
         if self.grad_formulation == 'analysis':
             if self.smaps is None:
-                return GradAnalysis(fourier_op, verbose=self.verbose)
+                return GradAnalysis(fourier_op=self.fourier_op,
+                                    verbose=self.verbose,
+                                    **kwargs)
             else:
-                return GradSelfCalibrationAnalysis(fourier_op=fourier_op,
-                                                           Smaps=self.smaps,
-                                                           verbose=self.verbose,
-                                                           )
+                return GradSelfCalibrationAnalysis(fourier_op=self.fourier_op,
+                                                   Smaps=self.smaps,
+                                                   verbose=self.verbose,
+                                                   **kwargs)
         elif self.grad_formulation == 'synthesis':
             if self.smaps is None:
-                return GradSynthesis(self.space_linear_op, fourier_op, self.verbose)
+                return GradSynthesis(linear_op=self.space_linear_op,
+                                     fourier_op=self.fourier_op,
+                                     verbose=self.verbose,
+                                     **kwargs)
             else:
-                return GradSelfCalibrationSynthesis(fourier_op, self.space_linear_op, self.smaps, self.verbose)
+                return GradSelfCalibrationSynthesis(fourier_op=self.fourier_op,
+                                                    linear_op=self.space_linear_op,
+                                                    Smaps=self.smaps,
+                                                    verbose=self.verbose,
+                                                    **kwargs)
         else:
             raise ValueError("Unknown Gradient formuation")
 
-    def reconstruct(self, kspace_data, x_init=None, max_iter_per_frame=15, reset_opt=True, recompute_smaps=False, smaps_kwargs=None,warm_x=True):
+    def reconstruct(self, kspace_data, x_init=None, max_iter_per_frame=15, reset_opt=True, recompute_smaps=False, grad_kwargs=None, smaps_kwargs=None, warm_x=True):
 
-        grad_op = self.get_grad_op()
+        grad_kwargs = dict() if grad_kwargs is None else grad_kwargs
+        grad_op = self.get_grad_op(**grad_kwargs)
 
-        if getattr(self.fourier_op.impl,'uses_sense', False) or self.smaps is not None:
+        if getattr(self.fourier_op.impl, 'uses_sense', False) or self.smaps is not None:
             if x_init is None:
-                x_init = np.squeeze(np.zeros(self.fourier_op.shape,dtype="complex64"))
-            final_estimate = np.zeros((len(kspace_data), *self.fourier_op.shape), dtype=x_init.dtype)
+                x_init = np.squeeze(
+                    np.zeros(self.fourier_op.shape, dtype="complex64"))
+            final_estimate = np.zeros(
+                (len(kspace_data), *self.fourier_op.shape), dtype=x_init.dtype)
         else:
             if x_init is None:
-                x_init = np.squeeze(np.zeros((self.fourier_op.n_coils, *self.fourier_op.shape),dtype="complex64"))
-            final_estimate = np.zeros((len(kspace_data), self.fourier_op.n_coils, *self.fourier_op.shape),dtype=x_init.dtype)
+                x_init = np.squeeze(
+                    np.zeros((self.fourier_op.n_coils, *self.fourier_op.shape), dtype="complex64"))
+            final_estimate = np.zeros(
+                (len(kspace_data), self.fourier_op.n_coils, *self.fourier_op.shape), dtype=x_init.dtype)
 
         if self.fourier_op.n_coils != kspace_data.shape[1]:
             raise ValueError("The kspace data should have shape N_frame x N_coils x N_samples. "
                              "Also, the provided number of coils should match.")
-        if smaps_kwargs is None and recompute_smaps == True:
+        if smaps_kwargs is None and recompute_smaps:
             smaps_kwargs = dict()
 
-        opt = self.initialize_opt(grad_op, x_init=x_init, synthesis_init=False, opt_kwargs={"cost":None}, metric_kwargs=dict())
+        opt = self.initialize_opt(grad_op, x_init=x_init, synthesis_init=False, opt_kwargs={
+                                  "cost": None}, metric_kwargs=dict())
         next_init = x_init
-        totalPB =  progressbar.ProgressBar(max_val=len(kspace_data))
+        totalPB = progressbar.ProgressBar(max_val=len(kspace_data))
         totalPB.start()
         for i in range(len(kspace_data)):
             # at each step a new frame is loaded
-            grad_op._obs_data=kspace_data[i,...]
+            grad_op._obs_data = kspace_data[i, ...]
             # reset Smaps and optimizer if required.
-            if recompute_smaps and (self.smaps is not None or getattr(self.fourier_op.impl,'use_sense', False)) :
-                Smaps, _ = get_Smaps(kspace_data[i,...],
-                                            img_shape=self.fourier_op.shape,
-                                            samples=self.fourier_op.samples,
-                                            min_samples=self.fourier_op.samples.min(axis=0),
-                                            max_samples=self.fourier_op.samples.max(axis=0),
-                                            density_comp=self.fourier_op.density_comp,
-                                            **smaps_kwargs)
-                if getattr(self.fourier_op.impl,'uses_sense', False):
+            if (recompute_smaps and
+                    (self.smaps is not None or getattr(self.fourier_op.impl, 'use_sense', False))):
+                Smaps, _ = get_Smaps(kspace_data[i, ...],
+                                     img_shape=self.fourier_op.shape,
+                                     samples=self.fourier_op.samples,
+                                     min_samples=self.fourier_op.samples.min(
+                                         axis=0),
+                                     max_samples=self.fourier_op.samples.max(
+                                         axis=0),
+                                     density_comp=self.fourier_op.density_comp,
+                                     **smaps_kwargs)
+                if getattr(self.fourier_op.impl, 'uses_sense', False):
                     grad_op.fourier_op.impl.operator.set_smaps(Smaps)
                 else:
                     grad_op.Smaps = Smaps
-                    
+
             if reset_opt:
-                opt = self.initialize_opt(grad_op, x_init=next_init,synthesis_init=False,  opt_kwargs={"cost":None}, metric_kwargs=dict())
-            # if no reset, the internal state is kept. (dual variable, dynamic step size)
+                opt = self.initialize_opt(grad_op,
+                                          x_init=next_init,
+                                          synthesis_init=False,
+                                          opt_kwargs={"cost": None},
+                                          metric_kwargs=dict())
+            # if no reset, the internal state is kept.
+            # (dual variable, dynamic step size)
             opt.iterate(max_iter=max_iter_per_frame)
 
             # Prepare for next iteration and save results
             if self.grad_formulation == "synthesis":
                 img = self.space_linear_op.adj_op(opt.x_final)
                 next_init = img if warm_x else x_init
-                final_estimate[i,...] = img
+                final_estimate[i, ...] = img
             else:
-                final_estimate[i,...] = opt.x_final
+                final_estimate[i, ...] = opt.x_final
                 next_init = self.opt_x_final if warm_x else x_init
             # Progressbar update
             clear_output(wait=True)
@@ -103,47 +124,59 @@ class SequentialFMRIReconstructor(BaseFMRIReconstructor):
 
 
 class ParallelFMRIReconstructor(SequentialFMRIReconstructor):
-    """ Parallel Reconstruction of fMRI data.
+    """
+    Parallel Reconstruction of fMRI data.
+
     Time frame are reconstructed independently, and in parallel to speed up the reconstruction
     """
 
-    def reconstruct(self, kspace_data, x_init=None, max_iter_per_frame=15,n_jobs=3, smaps_kwargs=None):
+    def reconstruct(self, kspace_data, x_init=None, max_iter_per_frame=15, n_jobs=3, smaps_kwargs=None):
 
-        def oneframe(kspace_data, idx, x_init, fourier_kwargs, grad_kwargs, smaps_kwargs, linear_op, prox_op, opt_name):
-            kspace_data=kspace_data[idx,...]
+        def oneframe(kspace_data, idx, x_init, fourier_kwargs, grad_kwargs,
+                     smaps_kwargs, linear_op, prox_op, opt_name):
+            kspace_data = kspace_data[idx, ...]
             fourier_cls = fourier_kwargs.pop('__class__')
             fourier_kwargs_kwargs = fourier_kwargs.pop('kwargs')
-            fourier_op=fourier_cls(**fourier_kwargs,**fourier_kwargs_kwargs)
+            fourier_op = fourier_cls(**fourier_kwargs, **fourier_kwargs_kwargs)
 
             if smaps_kwargs:
                 Smaps, _ = get_Smaps(kspace_data,
-                                            img_shape=fourier_op.shape,
-                                            samples=fourier_op.samples,
-                                            min_samples=fourier_op.samples.min(axis=0),
-                                            max_samples=fourier_op.samples.max(axis=0),
-                                            density_comp=fourier_op.density_comp,
-                                            **smaps_kwargs)
-                if getattr(fourier_op.impl,'uses_sense', False):
+                                     img_shape=fourier_op.shape,
+                                     samples=fourier_op.samples,
+                                     min_samples=fourier_op.samples.min(
+                                         axis=0),
+                                     max_samples=fourier_op.samples.max(
+                                         axis=0),
+                                     density_comp=fourier_op.density_comp,
+                                     **smaps_kwargs)
+                if getattr(fourier_op.impl, 'uses_sense', False):
                     fourier_op.impl.operator.set_smaps(Smaps)
                 else:
                     grad_kwargs['Smaps'] = Smaps
 
             grad_cls = grad_kwargs.pop('__class__')
-            grad_op=grad_cls(fourier_op=fourier_op, **grad_kwargs,num_check_lips=20)
+            grad_op = grad_cls(fourier_op=fourier_op, **
+                               grad_kwargs, num_check_lips=20)
 
-            opt = initialize_opt(opt_name, grad_op, linear_op, prox_op, x_init=x_init,
-                                 synthesis_init=False, opt_kwargs={"cost":None}, metric_kwargs=dict())
+            opt = initialize_opt(opt_name, grad_op, linear_op, prox_op,
+                                 x_init=x_init,
+                                 synthesis_init=False,
+                                 opt_kwargs={"cost": None},
+                                 metric_kwargs=dict())
             opt.iterate(max_iter=max_iter_per_frame)
-            if hasattr(grad_op,'linear_op'):
+            if hasattr(grad_op, 'linear_op'):
                 return grad_op.linear_op.adj_op(opt.x_final)
             else:
                 return opt.x_final
 
         # 1/ setup initial variable and final array
-        if (getattr(self.fourier_op.impl,'uses_sense', False) or self.smaps is not None) and x_init is None:
-            x_init = np.squeeze(np.zeros(self.fourier_op.shape,dtype="complex64"))
+        if x_init is None and  \
+           (getattr(self.fourier_op.impl, 'uses_sense', False) or self.smaps is not None):
+            x_init = np.squeeze(
+                np.zeros(self.fourier_op.shape, dtype="complex64"))
         elif x_init is None:
-            x_init = np.squeeze(np.zeros((self.fourier_op.n_coils, *self.fourier_op.shape),dtype="complex64"))
+            x_init = np.squeeze(
+                np.zeros((self.fourier_op.n_coils, *self.fourier_op.shape), dtype="complex64"))
 
         if self.fourier_op.n_coils != kspace_data.shape[1]:
             raise ValueError("The kspace data should have shape N_frame x N_coils x N_samples. "
@@ -152,10 +185,11 @@ class ParallelFMRIReconstructor(SequentialFMRIReconstructor):
         if smaps_kwargs is None:
             smaps_kwargs = dict()
         # 2/ extract info from fourier and grad-op to properly clone them
-        fourier_attrs=['__class__','shape','samples','n_coils']
-        if isinstance(self.fourier_op,NonCartesianFFT):
-            fourier_attrs += ['implementation','density_comp','kwargs']
-        fourier_kwargs={attr: copy.deepcopy(getattr(self.fourier_op,attr)) for attr in fourier_attrs}
+        fourier_attrs = ['__class__', 'shape', 'samples', 'n_coils']
+        if isinstance(self.fourier_op, NonCartesianFFT):
+            fourier_attrs += ['implementation', 'density_comp', 'kwargs']
+        fourier_kwargs = {attr: copy.deepcopy(
+            getattr(self.fourier_op, attr)) for attr in fourier_attrs}
 
         grad_kwargs = {'verbose': self.verbose}
 
@@ -179,10 +213,13 @@ class ParallelFMRIReconstructor(SequentialFMRIReconstructor):
         opt_name = copy.deepcopy(self.opt_name)
 
         # 3/ Reconstruct each time frame independently
-        results = Parallel(n_jobs=n_jobs,verbose=self.verbose)(delayed(oneframe)(kspace_data, j,
-                                                     x_init,
-                                                     fourier_kwargs, grad_kwargs, smaps_kwargs,
-                                                     linear_op,prox_op, opt_name,
-                                                     ) for j in range(len(kspace_data)))
+        results = Parallel(n_jobs=n_jobs, verbose=self.verbose)(
+            delayed(oneframe)(kspace_data, j,
+                              x_init,
+                              fourier_kwargs,
+                              grad_kwargs, smaps_kwargs,
+                              linear_op, prox_op, opt_name,
+                              )
+            for j in range(len(kspace_data)))
         final_estimates = np.ascontiguousarray(results)
         return final_estimates
