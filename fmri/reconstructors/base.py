996ba6ebbd80a67f8747ea6d849ecc6baa722a45
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2021-11-29 08:50:18 +0100

    add doc and formatting

--- fmri/reconstructors/base.py
+++ fmri/reconstructors/base.py
@@ -10,6 +10,8 @@ import warnings
 
 from modopt.opt.linear import Identity
 
+from .utils import initialize_opt
+
 
 OPTIMIZERS = {'pogm': 'synthesis',
               'fista':  'analysis',
@@ -42,14 +44,38 @@ class BaseFMRIReconstructor(object):
     """
 
     def __init__(self, fourier_op, space_linear_op, space_regularisation=None,
-                 time_linear_op=None, time_regularisation=None, Smaps=None,
-                 optimizer='pogm', verbose=0,):
-        """Instantiate Base reconstruction."""
+                 time_linear_op=None, time_regularisation=None, Smaps=None, optimizer='pogm', verbose=0,):
+        """
+        Create base reconstructor.
+
+        Parameters:
+        -----------
+        fourier_op: Fourier operator
+        space_linear_operator: Linear Operator
+            Linear operator for the in space regularisation.
+        space_regularisation: Proximity Operator, default Identity
+            Proximity Operator for the in space regularisation.
+        time_linear_op: Linear Operator
+            Linear operator for the  in-time regularisation.
+        time_regularisation: Proximity Operator, default Identity
+            Proximity Operator the in-time regularisation
+        Smaps: ndarray, default None
+            Sensibility map for the reconstruction (if not set in the fourier operator)
+        optimizer: "pogm" or "fista"
+            Optimizer algorithm to use.
+        verbose: int default=0
+            verbosity level.
+        """
         self.fourier_op = fourier_op
         self.space_linear_op = space_linear_op or Identity
         self.time_linear_op = time_linear_op or Identity
         self.opt_name = optimizer
         self.grad_formulation = OPTIMIZERS[optimizer]
+
+        if Smaps is not None:
+            if fourier_op.n_coils > 1:
+                if Smaps.shape[0] != fourier_op.n_coils and Smaps.shape[1:] != fourier_op.shape:
+                    raise("Smaps shape is different from the image dimensions.")
         self.smaps = Smaps
         self.verbose = verbose
 
@@ -68,5 +94,12 @@ class BaseFMRIReconstructor(object):
             self.time_prox_op = time_regularisation
 
     def reconstruct(self, kspace_data, *args, **kwargs):
-        """Reconstruct from the kspace_data."""
+        """Lauchn reconstruction."""
         raise NotImplementedError
+
+    def initialize_opt(self, grad_op, x_init=None, synthesis_init=False,
+                     opt_kwargs=None, metric_kwargs=None):
+        """Initialize an  optimizer for a given gradient """
+        return initialize_opt(opt_name=self.opt_name, grad_op=grad_op,
+                         linear_op=self.space_linear_op,
+                         prox_op=self.space_prox_op)
--- fmri/reconstructors/full.py
+++ fmri/reconstructors/full.py
@@ -6,37 +6,43 @@ from modopt.opt.linear import Identity
 from modopt.opt.proximity import LowRankMatrix, SparseThreshold
 
 from .base import BaseFMRIReconstructor
-from ..operators.fourier import SpaceFourier, TimeFourier
+from ..operators.fourier import TimeFourier
 
-
-
-class GlobalFMRIReconstructor(BaseFMRIReconstructor):
-    """ Global FMRI reconstructor"""
-
-    def initialize_opt(self, opt_kwargs, metric_kwargs):
-        raise NotImplementedError
-
-
-
-class LowRankPlusSparseFMRIReconstructor(GlobalFMRIReconstructor):
+class LowRankPlusSparseFMRIReconstructor(BaseFMRIReconstructor):
     """ This Class implement the reconstruction proposed in Petrov et al.
     https://doi.org/10.1016/j.neuroimage.2017.06.004
     """
-    def __init__(self, fourier_op, lowrank_thresh=1e-3,sparse_thresh=1e-3, Smaps=None):
-        super(GlobalFMRIReconstructor, self).__init__(fourier_op=fourier_op,
-                                                      space_linear_op=Identity(),
-                                                      space_regularisation=LowRankMatrix(lowrank_thresh,
-                                                                                         thresh_type="soft"),
-                                                      time_linear_op=TimeFourier(),
-                                                      time_regularisation=SparseThreshold(Identity(),
-                                                                                         sparse_thresh,
-                                                                                         thresh_type="soft"),
-                                                      Smaps=Smaps
-                                                      )
 
+    def __init__(self, fourier_op, lowrank_thresh=1e-3, sparse_thresh=1e-3, Smaps=None):
+        """
+        Instanciate LR+S Reconstruction.
 
-    def reconstruct(self, kspace_data, max_iter=30,eps=1e-5):
-        M = np.zeros((len(kspace_data),*self.fourier_op.img_shape),dtype="complex128")
+        Parameters:
+        -----------
+        fourier_op: SpaceFourierOperator
+            The fourier operator the fmri data
+        lowrank_thresh: float, default=1e-3
+            threshold for the singular value threshold operator.
+        sparse_thres: float, default=1e-3
+            threshold for the soft-thresholding operator.
+        Smaps: ndarray
+
+        """
+
+        super(BaseFMRIReconstructor, self).__init__(fourier_op=fourier_op,
+                                                    space_linear_op=Identity(),
+                                                    space_regularisation=LowRankMatrix(lowrank_thresh,
+                                                                                       thresh_type="soft"),
+                                                    time_linear_op=TimeFourier(),
+                                                    time_regularisation=SparseThreshold(Identity(),
+                                                                                        sparse_thresh,
+                                                                                        thresh_type="soft"),
+                                                    Smaps=Smaps
+                                                    )
+
+    def reconstruct(self, kspace_data, max_iter=30, eps=1e-5):
+        M = np.zeros((len(kspace_data), *self.fourier_op.img_shape),
+                     dtype="complex128")
 
         L = M.copy()
         S = M.copy()
@@ -44,26 +50,29 @@ class LowRankPlusSparseFMRIReconstructor(GlobalFMRIReconstructor):
         if self.smaps is None:
             M_old = self.fourier_op.adj_op(kspace_data)
         else:
-            M_old = np.sum(np.conjugate(self.smaps)*self.fourier_op.adj_op(kspace_data),axis=1)
+            M_old = np.sum(np.conjugate(self.smaps) *
+                           self.fourier_op.adj_op(kspace_data), axis=1)
         L_old = L.copy()
         S_old = S.copy()
 
-
-
         for i in tqdm.tqdm(range(max_iter)):
             # singular value soft thresholding
-            L = self.space_prox_op.op((M_old-S_old).reshape(self.fourier_op.n_frames,np.prod(self.fourier_op.img_shape)))
-            L = np.reshape(L,(self.fourier_op.n_frames, *self.fourier_op.img_shape))
+            L = self.space_prox_op.op(
+                (M_old-S_old).reshape(self.fourier_op.n_frames, np.prod(self.fourier_op.img_shape)))
+            L = np.reshape(L, (self.fourier_op.n_frames,
+                           *self.fourier_op.img_shape))
 
             # Soft thresholding in the time sparsifying domain
-            S = self.time_linear_op.adj_op(self.time_prox_op.op(self.time_linear_op.op(M_old-L_old)))
+            S = self.time_linear_op.adj_op(
+                self.time_prox_op.op(self.time_linear_op.op(M_old-L_old)))
             # Data consistency: substract residual
             if self.smaps is None:
-                M = L +S - self.fourier_op.adj_op(self.fourier_op.op(L+S)-kspace_data)
+                M = L + S - \
+                    self.fourier_op.adj_op(self.fourier_op.op(L+S)-kspace_data)
             else:
                 M = L + S - np.sum(np.conjugate(self.smaps) * self.fourier_op.adj_op(
-                                                                self.fourier_op.op((L+S)[:,np.newaxis,...] * self.smaps) - kspace_data),
-                                axis=1)
+                    self.fourier_op.op((L+S)[:, np.newaxis, ...] * self.smaps) - kspace_data),
+                    axis=1)
             if np.linalg.norm(L+S - L_old - S_old) <= eps * np.linalg.norm(L_old+S_old):
                 print("convergence reached")
                 break
