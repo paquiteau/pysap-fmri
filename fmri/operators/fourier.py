66072b13dedbae6fd58a76e492a9b9f5f42920dc
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2022-05-11 11:48:44 +0200

    Use MRICUfinufft as backend

--- fmri/operators/fourier.py
+++ fmri/operators/fourier.py
@@ -1,9 +1,9 @@
 """Fourier Operator for fMRI data."""
 import numpy as np
 import scipy as sp
-from mri.operators.fourier.non_cartesian import NonCartesianFFT
-from mri.operators.fourier.utils import estimate_density_compensation_gpu, estimate_density_compensation
-import copy
+import cupy as cp
+
+from mriCufinufft import MRICufiNUFFT
 
 
 class SpaceFourier:
@@ -27,70 +27,66 @@ class SpaceFourier:
     fourier_ops: list
         List of NonCartesianFFT Operator
     """
-    def __init__(self, samples, shape, n_coils=1, n_frames=None, estimate_density="gpu", smaps=None,  **kwargs):
+
+    def __init__(self, samples, shape, n_coils=1, n_frames=1, smaps=None, smaps_cached=True, estimate_density=True,  **kwargs):
         self.n_frames = n_frames
         self.n_coils = n_coils
         self.smaps = smaps
         self.shape = shape
 
+        repeat = False
         if samples.ndim == 2 and n_frames is None:
-            raise ValueError("2D array of samples provided, but n_frames is not specified.")
+            raise ValueError(
+                "2D array of samples provided, but n_frames is not specified.")
         if samples.ndim == 2:
             self.samples = np.repeat(samples[None, ...], n_frames, axis=0)
+            repeat = True
         elif samples.ndim == 3:
             self.samples = samples
         else:
             raise ValueError("samples array should be 2D or 3D.")
 
-        if estimate_density == "gpu":
-            edc = estimate_density_compensation_gpu
-        elif estimate_density == "cpu":
-            edc = estimate_density_compensation
+        if estimate_density and repeat:
+            print("repeat")
+            density = MRICufiNUFFT.estimate_density(samples, self.shape, n_iter=20)
         else:
-            raise ValueError("'estimate_density' should be 'gpu' or 'cpu'. ")
-
-        self.samples = np.require(self.samples, requirements=['OWNDATA','ENSUREARRAY'])
-
-        # print(self.samples.flags)
-        # print(self.samples.data)
-        # print(self.samples.shape)
-        # print(self.samples.strides)
-        # Create fourier operator for each frame
+            density = estimate_density
         self.fourier_ops = []
         for i in range(n_frames):
-            self.fourier_ops.append(NonCartesianFFT(self.samples[i],
-                                                    shape,
-                                                    n_coils=n_coils,
-                                                    density_comp=edc(self.samples[i], shape),
-                                                    implementation="gpuNUFFT",
-                                                    smaps=smaps,
-                                                    **kwargs,
-                                                    ))
+            self.fourier_ops.append(MRICufiNUFFT(
+                self.samples[i],
+                shape,
+                n_coils=n_coils,
+                smaps=cp.array(smaps, copy=False) if smaps is not None and smaps_cached else smaps,
+                smaps_cached=smaps_cached,
+                density=density,
+                **kwargs,
+            ))
 
     def op(self, data):
         """Forward Operator method."""
-
-        adj_data = np.squeeze(np.zeros((self.n_frames, self.n_coils, *self.shape),dtype='complex64'))
+        adj_data = np.squeeze(
+            np.zeros((self.n_frames, self.n_coils, *self.shape),
+                     dtype='complex64'))
         for i in range(self.n_frames):
             adj_data[i] = self.fourier_ops[i].op(data[i])
         return adj_data
 
     def adj_op(self, adj_data):
         """Adjoint Operator method."""
-
-        # print(adj_data[0].flags)
-        # print(adj_data[0].data)
-        # print(adj_data[0].shape)
-        # print(adj_data[0].strides)
         if self.smaps is None:
-            data  = np.squeeze(np.zeros((self.n_frames, self.n_coils, *self.shape),dtype=adj_data.dtype))
+            data = np.squeeze(
+                np.zeros((self.n_frames, self.n_coils, *self.shape),
+                         dtype=adj_data.dtype))
         else:
 
-            data  = np.squeeze(np.zeros((self.n_frames, *self.shape),dtype=adj_data.dtype))
+            data = np.squeeze(
+                np.zeros((self.n_frames, *self.shape), dtype=adj_data.dtype))
 
         for i in range(self.n_frames):
             data[i] = self.fourier_ops[i].adj_op(adj_data[i])
         return data
+
 
 class TimeFourier:
     """Temporal Fourier Transform on fMRI data."""
@@ -102,7 +98,7 @@ class TimeFourier:
     def op(self, x):
         """Forward Operator method..
 
-        Apply the fourier transform on the time axis, voxel wise
+        Apply the fourier transform on the time axis, voxel wise.
         """
         y = np.zeros_like(x)
         if self.roi is not None:
