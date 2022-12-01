07813b853f418f2cae0663fc1f8092ebcd9dd7e1
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2022-12-01 14:01:00 +0100

    add Global low rank reconstructor.

--- fmri/reconstructors/time_aware.py
+++ fmri/reconstructors/time_aware.py
@@ -2,12 +2,70 @@
 
 import numpy as np
 import tqdm
+from modopt.opt.algorithms import ForwardBackward
 from modopt.opt.linear import Identity
 from modopt.opt.proximity import SparseThreshold
 
 from ..operators.fourier import TimeFourier
 from ..operators.svt import FlattenSVT
+from ..operators.utils import make_gradient_operator
 from .base import BaseFMRIReconstructor
+
+
+class LowRankFMRIReconstructor(BaseFMRIReconstructor):
+    """
+    Reconstruct fMRI data with a global low rank apriori.
+
+
+    Parameters
+    ----------
+    fourier_op: SpaceFourierOperator
+    lowrank_thresh: float
+    roi: numpy.ndarray
+    """
+
+    def __init__(self, fourier_op, lowrank_thresh, lipschitz_cst=None, roi=None):
+        super().__init__(
+            fourier_op=fourier_op,
+            space_linear_op=Identity(),
+            space_prox_op=FlattenSVT(
+                threshold=lowrank_thresh,
+                initial_rank=5,
+                thresh_type="soft",
+                roi=roi,
+            ),
+        )
+        self.lipschitz_cst = lipschitz_cst
+
+    def reconstruct(
+        self,
+        kspace_data,
+        max_iter=50,
+        x_init=None,
+        **kwargs,
+    ):
+        """Reconstruct fmri data using fista."""
+        grad = make_gradient_operator(self.fourier_op, kspace_data)
+
+        if self.lipschitz_cst is None:
+            # TODO Compute the lipschitz cst using power method.
+            # Be robust to non cartesian.
+            self.lipschitz_cst = 1
+
+        if x_init is None:
+            x_init = self.fourier_op.adj_op(kspace_data)
+        opt = ForwardBackward(
+            x=x_init,
+            grad=grad,
+            prox=self.space_prox_op,
+            linear=self.space_linear_op,
+            beta_param=1 / self.lipschitz_cst,
+            lambda_param=kwargs.pop("lambda_param", 1.0),
+            auto_iterate=False,
+            **kwargs,
+        )
+        opt.iterate(max_iter)
+        return opt.x_final
 
 
 class LowRankPlusSparseFMRIReconstructor(BaseFMRIReconstructor):
