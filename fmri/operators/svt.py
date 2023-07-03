54113c05623440600704de3bc4e437192143f464
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2023-07-03 11:03:25 +0200

    refactor: remove the roi support, and add full rank support.

--- fmri/operators/svt.py
+++ fmri/operators/svt.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 """Singular Value Threshold operator."""
-import warnings
 import logging
 
 import numpy as np
 import scipy as sp
-from modopt.opt.linear import Identity
 from modopt.opt.proximity import ProximityParent
 from modopt.signal.noise import thresh
 
@@ -51,39 +49,49 @@ class SingularValueThreshold(ProximityParent):
         data_thresholded: ndarray
             The data with thresholded singular values.
         """
-        max_rank = min(data.shape) - 2
-        if self._rank > max_rank:
-            logger.warn("initial rank bigger than maximal possible one, updating.")
+        if self._rank is None:
+            U, S, V = sp.linalg.svd(data, full_matrices=False)
+            St = thresh(S, np.max(S) * self._threshold, self._threshold_type)
 
-        compute_rank = min(self._rank + 1, max_rank)
-        # Singular value are  in increasing order !
-        U, S, V = sp.sparse.linalg.svds(data, k=compute_rank)
-
-        if self._rel_thresh:
-            thresh_val = self._threshold * S[-1]
+            return (U * St) @ V
         else:
-            thresh_val = self._threshold
-        # increase the computational rank until we found a singular value small enought.
-        while (
-            thresh(S[0], thresh_val, self._threshold_type) > 0
-            and compute_rank < max_rank
-        ):
-            compute_rank = min(compute_rank + self._incre, max_rank)
+            max_rank = min(data.shape) - 2
+            if self._rank > max_rank:
+                logger.warn("initial rank bigger than maximal possible one, updating.")
+
+            compute_rank = min(self._rank + 1, max_rank)
+            # Singular value are  in increasing order !
             U, S, V = sp.sparse.linalg.svds(data, k=compute_rank)
-            logger.debug(f"increasing rank to {compute_rank}")
 
-        S = thresh(S, thresh_val, self._threshold_type)
-        self._rank = np.count_nonzero(S)
-        logger.debug(f"new Rank: {self._rank}, max value: {np.max(S)}")
+            if self._rel_thresh:
+                thresh_val = self._threshold * np.max(S)
+            else:
+                thresh_val = self._threshold
+            # increase the computational rank until we found a singular value small enought.
+            while (
+                thresh(np.min(S), thresh_val, self._threshold_type) > 0
+                and compute_rank < max_rank
+            ):
+                compute_rank = min(compute_rank + self._incre, max_rank)
+                U, S, V = sp.sparse.linalg.svds(data, k=compute_rank)
+                logger.debug(f"increasing rank to {compute_rank}")
 
-        return (U[:, -self._rank :] * S[-self._rank :]) @ V[-self._rank :, :]
+            S = thresh(S, thresh_val, self._threshold_type)
+            self._rank = np.count_nonzero(S)
+            logger.debug(f"new Rank: {self._rank}, max value: {np.max(S)}")
+
+            return (U[:, -self._rank :] * S[-self._rank :]) @ V[-self._rank :, :]
 
     def cost(self, data):
         """Compute cost of low rank operator.
 
         This is the nuclear norm of data.
         """
-        return np.sum(np.abs(sp.linalg.svd(data, compute_uv=False)))
+        if self._rank is None:
+            return self._threshold * np.sum(sp.linalg.svdvals(data))
+        return self._threshold * np.sum(
+            sp.sparse.linalg.svds(data, k=self._rank, return_singular_vectors=False)
+        )
 
 
 class RankConstraint(ProximityParent):
@@ -130,25 +138,15 @@ class FlattenSVT(SingularValueThreshold):
     SingularValueThreshold: Singular Value Threshold operator.
     """
 
-    def __init__(self, threshold, initial_rank, roi=None, thresh_type="hard-rel"):
+    def __init__(self, threshold, initial_rank, thresh_type="hard-rel"):
         super().__init__(threshold, initial_rank, thresh_type=thresh_type)
-        self.roi = roi
 
-    def op(self, data, roi=None, extra_factor=1.0):
+    def op(self, data, extra_factor=1.0):
         """Operator function."""
-        roi = roi or self.roi
-        if roi is not None:
-            roi_data = data[:, roi]
-            roi_results = super().op(roi_data, extra_factor)
-            results = np.zeros_like(data)
-            results[:, roi] = roi_results
-            return results
-
-        else:
-            shape = data.shape
-            results = super().op(data.reshape(shape[0], -1), extra_factor)
-            results = np.reshape(results, data.shape)
-            return results
+        shape = data.shape
+        results = super().op(data.reshape(shape[0], -1), extra_factor)
+        results = np.reshape(results, data.shape)
+        return results
 
     def cost(self, data):
         """Compute cost."""
