8b9db2c05027098364906d524bdd2d2b2486748b
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2023-07-28 16:55:42 +0200

    remove prints and add abstract interface.

--- fmri/operators/fourier.py
+++ fmri/operators/fourier.py
@@ -1,11 +1,26 @@
 """Fourier Operator for fMRI data."""
 
 import numpy as np
+from abc import ABC, abstractmethod
 
 from .fft import fft, ifft
 
 
-class CartesianSpaceFourier:
+class SpaceFourierBase(ABC):
+    @abstractmethod
+    def op(self, img):
+        pass
+
+    @abstractmethod
+    def adj_op(self, data):
+        pass
+
+    @property
+    def uses_sense(self) -> bool:
+        return self.smaps is not None
+
+
+class CartesianSpaceFourier(SpaceFourierBase):
     """A Fourier Operator in space."""
 
     def __init__(self, shape, n_frames, mask, n_coils=1, smaps=None):
@@ -35,11 +50,9 @@ class CartesianSpaceFourier:
             raise ValueError("Mask shape not understood")
         self.n_coils = n_coils
         self.smaps = smaps
-        self.uses_sense = smaps is not None
 
     def op(self, img):
         axes = tuple(range(-len(self.shape), 0))
-        print("global", axes, img.shape)
         if self.n_coils > 1:
             if self.smaps is not None:
                 img2 = np.repeat(img[:, np.newaxis, ...], self.n_coils, axis=1)
@@ -47,11 +60,9 @@ class CartesianSpaceFourier:
                 ksp = fft(img2, axis=axes)
             else:
                 ksp = fft(img, axis=axes)
-            print("global", ksp.shape, self.mask.shape)
             return ksp * self.mask[:, np.newaxis, ...]
         else:
             ksp = fft(img, axis=axes)
-            print("global", ksp.shape, self.mask.shape, "n_coils=1")
             return ksp * self.mask
 
     def adj_op(self, kspace_data):
@@ -77,9 +88,9 @@ class CartesianSpaceFourier:
             return ifft(kspace_data, axis=axes)
 
 
-class RepeatOperator:
+class RepeatOperator(SpaceFourierBase):
     def __init__(self, fourier_ops):
-        self.fourier_ops = fourier_ops
+        self.fourier_ops = list(fourier_ops)
 
     def op(self, data):
         return np.asarray(
@@ -97,14 +108,25 @@ class RepeatOperator:
             ]
         )
 
+    @property
+    def shape(self):
+        return self.fourier_ops[0].shape
 
-class FFT_Sense:
+    @property
+    def n_frames(self):
+        return len(self.fourier_ops)
+
+    @property
+    def smaps(self):
+        return self.fourier_ops[0].smaps
+
+
+class FFT_Sense(SpaceFourierBase):
     def __init__(self, shape, n_coils, mask, smaps):
         self.shape = shape
         self.n_coils = n_coils
         self.mask = mask
         self.smaps = smaps
-        self.uses_sense = smaps is not None
 
     def op(self, img):
         axes = tuple(range(-len(self.shape), 0))
